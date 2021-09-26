cdfd06e0bc1392df3ee209dcbdeaa78ab3276ae6
Roby Attoillah <byrocuy@gmail.com>
2021-09-26 22:19:22 +0700

    Added edit and update feature

--- controllers/PegawaiControllers.js
+++ controllers/PegawaiControllers.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+const kecamatan = require('../models/kecamatan');
+const pegawai = require('../models/pegawai');
 var Pegawai = require('../models/pegawai');
 
 var PegawaiController = {};
@@ -37,6 +39,37 @@ PegawaiController.delete = function(req, res){
 }
 
 // Edit instance pegawai
+PegawaiController.edit = function(req, res){
+    var id = req.params._id;
+    Pegawai.findOne({ _id: id }, function(err, pegawai){
+        if(pegawai){
+            console.log(pegawai);
+            kecamatan.find({}, function(err, kec){
+                kecamatan.findOne({ _id:pegawai.id_kec }, function(err, x){
+                    console.log(x);
+                    res.render('editpegawai', {pegawai: pegawai, title: 'CRUD', kecamatan: kec, x: x});
+                });
+            });
+        } else{
+            res.redirect('../');
+        }
+    })
+}
 
+PegawaiController.update = function(req, res){
+    pegawai.findByIdAndUpdate(req.params._id, {
+        $set: { nama: req.body.nama,
+        email: req.body.email,
+        umur: req.body.umur,
+        id_kec: req.body.id_kec
+    }
+    }, { new: true }, function(err, pegawai){
+        if(err){
+            console.log(err);
+            res.render('editpegawai', {pegawai: req.body});
+        }
+        res.redirect('http://localhost:3000')
+    })
+}
 
 module.exports = PegawaiController;
